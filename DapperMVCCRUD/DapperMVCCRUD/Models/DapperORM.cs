578b4a86741bbcd9e920e6f2e06f4def04825d83
anvesh25 <anveshbabu.26@gmail.com>
2018-07-16 23:30:11 +0530

    Created CRUD MVC application by using Dapper

---
+++ DapperMVCCRUD/DapperMVCCRUD/Models/DapperORM.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DapperMVCCRUD.Models
+{
+    public class DapperORM
+    {
+        private static string connectionString = @"Data Source=ANVESH\SQLSERVER; Initial Catalog=DapperDB; Integrated Security=True;";
+
+        public static void ExecuteWithoutReturn(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                sqlCon.Execute(procedureName, param, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        //DapperORM.ExecuteReturnScalar<int>(_,_);
+        public static T ExecuteReturnScalar<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                return (T)Convert.ChangeType(sqlCon.ExecuteScalar(procedureName, param, commandType: CommandType.StoredProcedure), typeof(T));
+            }
+        }
+
+        //DapperORM.ReturnList<EmployeeModel> <= IEnumerable<EmployeeModel>
+        public static IEnumerable<T> ReturnList<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                return sqlCon.Query<T>(procedureName, param, commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
